8d4864a2cd379918774948f0fce3b247449dcfcd
brucetangdesign <bruce@brucetangdesign.com>
2020-02-02 23:22:40 -0500

    added mines to tiles

--- board.rb
+++ board.rb
@@ -6,29 +6,30 @@ class Board
 
     def initialize(grid_size, num_mines = 10)
         @grid = []
+        mine_positions = set_mine_positions(grid_size, num_mines)
 
         (0...grid_size).each do |i|
             @grid << []
             (0...grid_size).each do |j|
-                @grid[i] << Tile.new()
+                val = mine_positions.include?([i,j]) ? "mine" : ""
+                @grid[i] << Tile.new(val)
             end
         end
-
-        set_mine_positions(num_mines)
     end
 
-    def set_mine_positions(num_mines)
+    def set_mine_positions(grid_size, num_mines)
         possible_positions = []
         mine_positions = []
         # store all possible positions
-        (0...@grid.count).each { |i| (0...@grid.count).each { |j| possible_positions << [i,j] } }
-
-        #get a random setopf coordinates to place mines at
+        (0...grid_size).each { |i| (0...grid_size).each { |j| possible_positions << [i,j] } }
+        #get a random set of coordinates to place mines at
         num_mines.times do
             random_position = possible_positions.sample
             mine_positions << random_position
             possible_positions.delete(random_position)
         end
+
+        mine_positions
     end
 
     def render
--- tile.rb
+++ tile.rb
@@ -1,14 +1,17 @@
 require "colorize"
 
 class Tile
-    attr_reader :value, :face
-    def initialize
-        @value = ""
+    attr_reader :value, :face, :flagged
+    MINE_CHAR = "\u2297"
+
+    def initialize(value="")
+        @value = (value == "mine") ? MINE_CHAR : value
         @face = "\u2588"
+        @flagged = false
     end
 
     def flag
-        @face = "\u0224B"
+        @face = "\u224B"
     end
 
     def reveal
