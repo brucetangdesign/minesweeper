61e26df24bbef0c1051d0400f07cf2e4b9b6df62
brucetangdesign <bruce@brucetangdesign.com>
2020-02-08 23:36:21 -0500

    finsihed faces/values for tiles

--- board.rb
+++ board.rb
@@ -7,11 +7,12 @@ class Board
     def initialize(grid_size, num_mines = 10)
         @grid = []
         mine_positions = set_mine_positions(grid_size, num_mines)
+        #mine_neighbors = get_mine_neighbors(mine_positions, grid_size)
 
         (0...grid_size).each do |i|
             @grid << []
             (0...grid_size).each do |j|
-                val = mine_positions.include?([i,j]) ? "mine" : ""
+                val = mine_positions.include?([i,j]) ? "mine" : "clear"
                 @grid[i] << Tile.new(val)
             end
         end
@@ -22,6 +23,7 @@ class Board
         mine_positions = []
         # store all possible positions
         (0...grid_size).each { |i| (0...grid_size).each { |j| possible_positions << [i,j] } }
+
         #get a random set of coordinates to place mines at
         num_mines.times do
             random_position = possible_positions.sample
@@ -32,12 +34,14 @@ class Board
         mine_positions
     end
 
+    
+
     def render
         puts "  #{[*0...@grid.count].join(" ")}"
 
         (0...@grid.count).each do |i|
             row = @grid[i].map {|tile| tile.face}.join(" ")
-            puts "#{i} #{row.colorize(:light_black)}"
+            puts "#{i} #{row}"
         end
     end
 end
--- tile.rb
+++ tile.rb
@@ -2,9 +2,10 @@ require "colorize"
 
 class Tile
     attr_reader :value, :face, :flagged
-    MINE_CHAR = "\u2297"
+    MINE_CHAR = "\u2297".colorize(:red)
+    CLEAR_CHAR = "\u2588".colorize(:light_black)
 
-    def initialize(value="")
+    def initialize(value)
         @value = (value == "mine") ? MINE_CHAR : value
         @face = "\u2588"
         @flagged = false
