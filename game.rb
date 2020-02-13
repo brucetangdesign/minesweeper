b63234acd232fac162d479bb64ed3f1fdbd93b43
brucetangdesign <bruce@brucetangdesign.com>
2020-02-13 16:32:34 -0500

    added save functionality

--- game.rb
+++ game.rb
@@ -1,9 +1,49 @@
 require "colorize"
+require 'yaml'
 require_relative "./board.rb"
 
 class Game
     def initialize
         @board = Board.new(9)
+        @saving = false
+
+        prompt_user
+    end
+
+    def prompt_user
+        user_input = nil
+        valid_file = false
+
+        until user_input && (user_input == "new" || valid_file)
+            puts "Type \"new\" to start a new game. To load a saved game type the filename."
+            user_input = gets.chomp.strip
+
+            exit! if user_input == "exit"
+            run if user_input == "new"
+
+            case user_input
+            when "exit"
+                exit!
+            when "new"
+                run
+            else
+                begin
+                    valid_file = load_file(user_input)
+                rescue => e
+                    puts e.to_s.colorize(:red)
+                    puts ""
+
+                    valid_file = nil
+                end
+            end
+        end
+    end
+
+    def load_file(file_name)
+        raise "File not found" if !File.exists?(file_name)
+        raise "Invalid file (only .yml files are allowed)" if !file_name.include? (".yml")
+        @board = YAML.load(File.read(file_name))
+        run
     end
 
     def get_pos_and_move
@@ -14,6 +54,10 @@ class Game
             puts "\nEnter a coordinate separated by a comma, followed by a space and then -r to reveal or -f to flag (ex. 3,4 -r)"
             pos, move = gets.chomp.strip.split(" ")
             exit! if pos == "exit"
+            if pos == "save"
+                @saving = true
+                return
+            end
 
             begin
                 pos = parsePos(pos)
@@ -70,21 +114,25 @@ class Game
     end
 
     def run
-        play until won? || lost?
+        play until won? || lost? || @saving
         system("clear")
         @board.render
 
-        puts ""
-        puts "=".ljust(40,"=")
-        puts "=".ljust(40,"=")
-        puts ""
-        
-        puts "CONGRATULATIONS, YOU WON!" if won?
-        puts "BOOOOOOM, YOU'RE DEAD!" if lost?
-        
-        puts ""
-        puts "=".ljust(40,"=")
-        puts "=".ljust(40,"=")
+        if !@saving
+            puts ""
+            puts "=".ljust(40,"=")
+            puts "=".ljust(40,"=")
+            puts ""
+            
+            puts "CONGRATULATIONS, YOU WON!" if won?
+            puts "BOOOOOOM, YOU'RE DEAD!" if lost?
+            
+            puts ""
+            puts "=".ljust(40,"=")
+            puts "=".ljust(40,"=")
+        else
+            save_game
+        end
     end
 
     def won?
@@ -100,7 +148,19 @@ class Game
         @board.reveal_mines
         @board.render
     end
+
+    def save_game
+        puts ""
+        puts "=".ljust(40,"=")
+        puts "=".ljust(40,"=")
+        puts ""
+        puts "Enter a file name"
+        
+        file_name = gets.chomp.strip
+
+        File.open("#{file_name}.yml", "w") { |file| file.write(@board.to_yaml) }
+        exit!
+    end
 end
 
 game = Game.new
-game.run
